b0864ea7efb838286136663226cb370221eb470c
ndk9999 <nguyenduykhanh9999@live.com>
2021-12-27 23:06:36 -0800

    Create ProductApi project and migrate data

---
+++ BhrugenPatel/MangoRestaurant/Mango.Services.ProductApi/DbContexts/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Mango.Services.ProductApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mango.Services.ProductApi.DbContexts
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ BhrugenPatel/MangoRestaurant/Mango.Services.ProductApi/Models/Product.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Mango.Services.ProductApi.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Range(1, 1000)]
+        public decimal Price { get; set; }
+
+        public string Description { get; set; }
+
+        public string Category { get; set; }
+
+        public string ImageUrl { get; set; }
+    }
+}
