3cb3be01c1858ba2ab33fca8fe7cff7d186285ef
ndk9999 <nguyenduykhanh9999@live.com>
2022-01-19 01:34:05 -0800

    Add API Gateway using Ocelot

---
+++ BhrugenPatel/MangoRestaurant/Mango.GatewaySolution/Program.cs
@@ -0,0 +1,24 @@
+using Microsoft.IdentityModel.Tokens;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services
+    .AddAuthentication("Bearer")
+    .AddJwtBearer("Bearer", options =>
+    {
+        options.Authority = "https://localhost:7231";
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateAudience = false
+        };
+    });
+
+builder.Services.AddOcelot();
+
+var app = builder.Build();
+
+await app.UseOcelot();
+
+app.Run();
